972abc084a71def65d11b3c82def71aead071704
Eli Bishop <eli@launchdarkly.com>
2022-10-07 10:40:20 -0700

    update comment text to follow the pattern used in the other database packages

--- src/LaunchDarkly.ServerSdk.Consul/Consul.cs
+++ src/LaunchDarkly.ServerSdk.Consul/Consul.cs
@@ -13,14 +13,30 @@ namespace LaunchDarkly.Sdk.Server.Integrations
         public static readonly string DefaultPrefix = "launchdarkly";
 
         /// <summary>
-        /// Returns a builder object for creating a Consul-backed data store.
+        /// Returns a builder object for creating a Consul-backed persistent data store.
         /// </summary>
         /// <remarks>
-        /// This object can be modified with <see cref="ConsulDataStoreBuilder"/> methods for any desired
-        /// custom Consul options. Then, pass it to <see cref="Components.PersistentDataStore(IComponentConfigurer{IPersistentDataStoreAsync})"/>
-        /// and set any desired caching options. Finally, pass the result to <see cref="ConfigurationBuilder.DataStore(IComponentConfigurer{IDataStore})"/>.
-        /// </remarks>
-        /// <example>
+        /// <para>
+        /// You can use methods of the builder to specify any non-default Consul options
+        /// you may want, before passing the builder to
+        /// <see cref="Components.PersistentDataStore(IComponentConfigurer{IPersistentDataStore})"/>.
+        /// In this example, the store is configured with only the host address:
+        /// </para>
+        /// <code>
+        ///     var config = Configuration.Builder("sdk-key")
+        ///         .DataStore(
+        ///             Components.PersistentDataStore(
+        ///                 Consul.DataStore().Address("http://my-consul-host:8500")
+        ///             )
+        ///         )
+        ///         .Build();
+        /// </code>
+        /// <para>
+        /// Note that the SDK also has its own options related to data storage that are configured
+        /// at a different level, because they are independent of what database is being used. For
+        /// instance, the builder returned by <see cref="Components.PersistentDataStore(IComponentConfigurer{IPersistentDataStore})"/>
+        /// has options for caching:
+        /// </para>
         /// <code>
         ///     var config = Configuration.Builder("sdk-key")
         ///         .DataStore(
@@ -30,7 +46,8 @@ namespace LaunchDarkly.Sdk.Server.Integrations
         ///         )
         ///         .Build();
         /// </code>
-        /// </example>
+        /// </remarks>
+        /// <returns>a data store configuration object</returns>
         public static ConsulDataStoreBuilder DataStore() =>
             new ConsulDataStoreBuilder();
     }
